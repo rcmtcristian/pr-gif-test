2d3d0cf679c506165486f4b3e3c78fe27be246ea
rcmtcristian <rcmtcristian@gmail.com>
2023-10-12 21:15:25 -0400

    ✅ Changed based emotes

--- src/giphy.js
+++ src/giphy.js
@@ -1,14 +1,8 @@
 function getRandomEmote() {
-  const emotes = ["🫡"];
+  const emotes = ["😊", "🫡", "🥺", "🥳", "🤯", "😂", "😃", "🔥", "🤩", "✨"];
   const randomIndex = Math.floor(Math.random() * emotes.length);
   return emotes[randomIndex];
 }
-
-// function getRandomEmote() {
-//   const emotes = ["😊", "👍", "🙌", "👏", "🤔", "😂", "😃", "🔥", "💡", "✨"];
-//   const randomIndex = Math.floor(Math.random() * emotes.length);
-//   return emotes[randomIndex];
-// }
 
 const features = [
   `https://emojik.vercel.app/s/😎_${getRandomEmote()}?size=128`,
@@ -27,27 +21,7 @@ const reviewGifs = [
   "https://media.giphy.com/media/xUKrrEnN9I5lnrcSMv/giphy.gif",
 ];
 
-// const features = [
-//   "https://media.giphy.com/media/PiQejEf31116URju4V/giphy.gif",
-//   "https://media.giphy.com/media/LmNwrBhejkK9EFP504/giphy.gif",
-//   "https://media.giphy.com/media/349qKnoIBHK1i/giphy.gif",
-//   "https://media.giphy.com/media/5VKbvrjxpVJCM/giphy.gif",
-//   "https://media.giphy.com/media/oymRJRRiiPaVzDnIF1/giphy.gif",
-//   "https://media.giphy.com/media/PI2ZELDbBa4Bq/giphy.gif",
-// ];
-
-// const features = () => {
-//   const ranArray = randomArray();
-//   return ranArray;
-// };
-
-const fixes = [
-  "https://media.giphy.com/media/5bHIZ3ok4UpJS/giphy.gif",
-  "https://media.giphy.com/media/PrEUkNFD9pN2o/giphy.gif",
-  "https://media.giphy.com/media/xT1R9T9auEhJkm87Di/giphy.gif",
-  "https://media.giphy.com/media/3o6wreJw9IA7HRQ87u/giphy.gif",
-  "https://media.giphy.com/media/Ml9xFNKLMd8FW/giphy.gif",
-];
+const fixes = [`https://emojik.vercel.app/s/🤓_${getRandomEmote()}?size=128`];
 
 const defaults = [
   "https://media.giphy.com/media/l3q2wJsC23ikJg9xe/giphy.gif",
@@ -58,13 +32,7 @@ const defaults = [
   "https://media.giphy.com/media/pqcKfVvWRowVy/giphy.gif",
 ];
 // const defaults = [
-//   "https://media.giphy.com/media/l3q2wJsC23ikJg9xe/giphy.gif",
-//   "https://media.giphy.com/media/26gsjCZpPolPr3sBy/giphy.gif",
-//   "https://media.giphy.com/media/d68IdpvmAHohx5NMEV/giphy.gif",
-//   "https://media.giphy.com/media/WV9Xx9itqEIu8GQloO/giphy.gif",
-//   "https://media.giphy.com/media/xUA7aMYThXDVx4y5O0/giphy.gif",
-//   "https://media.giphy.com/media/pqcKfVvWRowVy/giphy.gif",
-// ];
+
 module.exports = {
   reviewGifs,
   features,
